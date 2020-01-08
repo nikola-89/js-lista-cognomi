75ac8c1c4cd88ea83d7ed9afeea6f6d3d0f9348a
Nicola <nicola.ba1989@gmail.com>
2020-01-08 16:56:15 +0100

    bonus capitalize

---
+++ bonus/js/script.js
@@ -0,0 +1,14 @@
+var cognomiLista = ['Alighieri', 'Petrarca', 'Manzoni', 'Leopardi', 'Carducci', 'Montale', 'Buonarroti' , 'Ungaretti', 'Pasolini'];
+
+var cognomeUser = prompt('Inserisci il tuo cognome:');
+
+while (cognomeUser.length == 2) {
+    cognomeUser = prompt('Inserisci il tuo cognome:');
+}
+cognomeUser = cognomeUser[0].toUpperCase() + cognomeUser.slice(1)
+cognomiLista.push(cognomeUser);
+cognomiLista.sort();
+
+for (var i = 0; i < cognomiLista.length; i++) {
+    document.getElementById('listaHtml').innerHTML += '<li>' + cognomiLista[i] + ' | ' + (i+1) + '</li>';
+}
